24a9e6e695f8173f6afeacf26ce37e5d0f73a309
root <gajanaruto@gmail.com>
2016-12-23 13:48:15 +0530

    :

--- modules/Key.py
+++ modules/Key.py
@@ -1,7 +1,9 @@
-import paramiko
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('192.168.1.44', username='pi', password='raspberry')
-stdin, stdout, stderr = ssh.exec_command("uptime")
-type(stdin)
-print(stdout.readlines())
+def run(**args):
+
+    import paramiko
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect('192.168.1.44', username='pi', password='raspberry')
+    stdin, stdout, stderr = ssh.exec_command("uptime")
+    type(stdin)
+    return stdout.readlines()
